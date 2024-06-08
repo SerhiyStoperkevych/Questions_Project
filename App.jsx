e0d9519ea6c2e7e969b8a9275d988f8b983b4e82
SerhiyStoperkevych <116464169+SerhiyStoperkevych@users.noreply.github.com>
2024-06-08 15:48:14 -0400

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Frequently Asked Questions</h1>
+
+      <nav>
+        <section>
+          <h3>What do I need to do?</h3>
+          <p>Just do it.</p>
+        </section>
+
+        <section>
+          <h3>How can I do it?</h3>
+          <p>Just do it.</p>
+        </section>
+
+        <section>
+          <h3>Why do I need to do it?</h3>
+          <p>Just do it.</p>
+        </section>
+
+        <section>
+          <h3>How long do I need to do it?</h3>
+          <p>Just do it.</p>
+        </section>
+
+        <section>
+          <h3>What does this mean?</h3>
+          <p>Just do it</p>
+        </section>
+      </nav>
+    </div>
+  )
+}
+
+export default App
